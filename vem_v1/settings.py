fb2ebc7255218228d15279e886a325867d659d1b
nexthem-1551 <gm179gaurav@gmail.com>
2019-03-31 14:37:08 +0530

    adding machine database

--- vem_v1/settings.py
+++ vem_v1/settings.py
@@ -122,3 +122,6 @@ STATIC_ROOT = ''
 STATICFILES_DIRS = ('static', )
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = ''
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- website/models.py
+++ website/models.py
@@ -30,3 +30,23 @@ class company(models.Model):
 	def __str__(self):
 		return self.state
 
+class machine(modles.Model):
+	added_by = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+	m_name = models.CharField(max_length=60, null=False, blank=False)
+	m_image = models.ImageField(upload_to="images/machines/", blank=True, default='defImages/defMachine.jpeg')
+	m_upload_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+	m_verified = models.BooleanField(default=False, null=True, blank=True)
+	m_avail = models.BooleanField(default=False, null=True, blank=True)
+	m_subtype = models.CharField(max_length=100, null=True, blank=True, default=None)
+	m_location = models.CharField(max_length=1000, null=False, blank=False)
+	m_brand = models.CharField(max_length=50, null=True, blank=True)
+	m_manufacturing_year = models.CharField(max_length=15, null=True, blank=True)
+	m_size = models.DecimalField(max_digits=30, decimal_places=10, default=0, blank=True, null=True)
+	m_travel_length = models.DecimalField(max_digits=30, decimal_places=10, default=0, blank=True, null=True)
+	m_accuracy = modles.DecimalField(max_digits=10, decimal_places=10, default=0, blank=True, null=True)
+	m_spindle_rpm = models.IntegerField(default=0, blank=True, null=True)
+	m_tonnage = models.FloatField(default=0, blank=True, null=True)
+	m_ra_value = models.DecimalField(max_digits=30, decimal_places=10, default=0, blank=True, null=True)
+	m_current_rating = models.DecimalField(max_digits=30, decimal_places=10, default=0, blank=True, null=True)
+	m_tie_bar = models.DecimalField(max_digits=30, decimal_places=10, default=0, blank=True, null=True)
+	m_shot_weight = models.DecimalField(max_digits=30, decimal_places=10, default=0, blank=True, null=True)
